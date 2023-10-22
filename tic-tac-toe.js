52578a124d5e402cc9300e3db8c06f6a96427a3f
ria876 <brianna.roper17@gmail.com>
2023-10-22 00:00:08 -0500

    Added Exercise two

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,9 +2,19 @@ window.onload = function(){
   
     let brd = document.getElementById("board");
     let cell = brd.getElementsByTagName("div");
+    let gameState = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
 
     for(let c = 0; c < 9; c++){
         cell[c].classList.add("square");
+        cell[c].addEventListener('click', function() {
+            if (gameState[c] === '') {
+                gameState[c] = currentPlayer;
+                cell[c].classList.add(currentPlayer);
+                cell[c].innerHTML = currentPlayer;
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     
     }
 }
